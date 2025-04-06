42c71439f5024c4429b5aa806efc28378fb9bd4f
Sukrit Boonin <sukrit_dinosuar@hotmail.com>
2025-04-06 23:15:15 +0700

    feat: New project structure for separate API module
    * changed all the project structure to separate API to an isolated maven module
    
    * new structure: discordsrv-plotsystem:discordsrv-plotsystem.pom -> discordsrv-plotsystem:discordps & discordsrv-plotsystem:discordps-api
    
    * the API module does not have any function yet

---
+++ discordps-api/src/main/java/github/tintinkung/discordps/api/DiscordPlotSystemAPI.java
@@ -0,0 +1,17 @@
+package github.tintinkung.discordps.api;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DiscordPlotSystemAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
