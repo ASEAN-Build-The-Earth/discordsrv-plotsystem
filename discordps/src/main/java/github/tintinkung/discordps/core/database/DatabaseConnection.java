4bd5d8282d51754b810eb3c57d22ef9f08e56c59
Sukrit Boonin <sukrit_dinosuar@hotmail.com>
2025-04-08 22:33:00 +0700

    feat: mysql support and optimize database initialization
    * Change database connection for mariadb to be connecting to MariaDbDataSource instead of direct driver
    
    * Added mysql support since the connector is already provided by discordSRV
    
    * Changed DatabaseConnection config setting to be java Properties
    
    * Added ManifestResourceTransformer to maven shade plugin to keep our application's manifest archives.

--- discordps/src/main/java/github/tintinkung/discordps/core/database/DatabaseConnection.java
+++ discordps/src/main/java/github/tintinkung/discordps/core/database/DatabaseConnection.java
@@ -1,5 +1,6 @@
 package github.tintinkung.discordps.core.database;
 
+import github.scarsz.discordsrv.dependencies.commons.lang3.StringUtils;
 import github.tintinkung.discordps.ConfigPaths;
 import github.tintinkung.discordps.DiscordPS;
 import com.zaxxer.hikari.HikariConfig;
@@ -11,33 +12,82 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DatabaseConnection {
-    private final static HikariConfig config = new HikariConfig();
+    // https://regex101.com/r/qtakRP/1
+    private final static Pattern JDBC_PATTERN = Pattern.compile("^(?<proto>\\w+):(?<engine>\\w+)://(?<host>.+?)(:(?<port>\\d{1,5}|PORT))?((/\\w+)+|/)\\??(?<params>.+)?$");
+
+    private final static Properties config = new Properties();
     private static HikariDataSource dataSource;
 
     private static int connectionClosed, connectionOpened;
 
-    public static void InitializeDatabase() throws ClassNotFoundException {
-        Class.forName("org.mariadb.jdbc.Driver"); // newer: com.mysql.cj.jdbc.Driver
-
+    public static boolean InitializeDatabase() throws ClassNotFoundException {
         FileConfiguration configFile = DiscordPS.getPlugin().getConfig();
         String URL = configFile.getString(ConfigPaths.DATABASE_URL);
         String name = configFile.getString(ConfigPaths.DATABASE_NAME);
         String username = configFile.getString(ConfigPaths.DATABASE_USERNAME);
         String password = configFile.getString(ConfigPaths.DATABASE_PASSWORD);
 
-        config.setJdbcUrl(URL + name);
-        config.setUsername(username);
-        config.setPassword(password);
-        config.addDataSourceProperty("cachePrepStmts", "true");
-        config.addDataSourceProperty("prepStmtCacheSize", "250");
-        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-        config.setMaximumPoolSize(3);
+        // Validate config
+        if(StringUtils.isBlank(URL)) return false;
+        Matcher matcher = JDBC_PATTERN.matcher(URL);
 
-        dataSource = new HikariDataSource(config);
+        if(!matcher.matches()) {
+            DiscordPS.error("Not using JDBC because the JDBC connection string is invalid!");
+            return false;
+        }
 
+        if(!matcher.group("proto").equalsIgnoreCase("jdbc")) {
+            DiscordPS.error("Not using JDBC because the protocol of the JDBC URL is wrong!");
+            return false;
+        }
+
+        // Decide SQL driver to choose, else HikariDataSource will try to automatically find it
+        String engine = matcher.group("engine");
+
+        // Provided by our plugin since Plot-System mainly use mariadb
+        if(engine.equalsIgnoreCase("mariadb")) {
+            Class.forName("org.mariadb.jdbc.MariaDbDataSource");
+
+            config.setProperty("dataSourceClassName", org.mariadb.jdbc.MariaDbDataSource.class.getName());
+            config.setProperty("dataSource.user", username);
+            config.setProperty("dataSource.password", password);
+            config.setProperty("dataSource.databaseName", name);
+            config.setProperty("maximumPoolSize", String.valueOf(3));
+
+            if(matcher.group("host") != null)
+                config.setProperty("dataSource.serverName", matcher.group("host"));
+            if(matcher.group("port") != null)
+                config.setProperty("dataSource.portNumber", matcher.group("port"));
+        }
+
+        // Provided by DiscordSRV
+        if(engine.equalsIgnoreCase("mysql")) {
+            // The MySQL DataSource is known to be broken with respect to network timeout support.
+            // Use jdbcUrl configuration with a specified driver instead.
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            config.setProperty("jdbcUrl", URL);
+            config.setProperty("username", username);
+            config.setProperty("password", password);
+            config.setProperty("dataSource.databaseName", name);
+            config.setProperty("dataSource.cachePrepStmts", "true");
+            config.setProperty("dataSource.prepStmtCacheSize", String.valueOf(250));
+            config.setProperty("dataSource.prepStmtCacheSqlLimit", String.valueOf(2048));
+            config.setProperty("driverClassName", com.mysql.cj.jdbc.Driver.class.getName());
+            config.setProperty("maximumPoolSize", String.valueOf(3));
+        }
+
+        dataSource = new HikariDataSource(new HikariConfig(config));
+        DiscordPS.info("Initialized database with: " + (
+            dataSource.getDriverClassName() == null?
+            dataSource.getDataSourceClassName()
+            : dataSource.getDriverClassName())
+        );
         createTables();
+        return true;
     }
 
     @Deprecated
@@ -187,7 +237,7 @@ public class DatabaseConnection {
 
             static {
                 tables = Collections.singletonList(
-                        // LangUser
+                        // plotsystem_discord_webhook
                         "CREATE TABLE IF NOT EXISTS `langUsers` (" +
                                 "`uuid` varchar(36) PRIMARY KEY," +
                                 "`lang` varchar(10)" +
