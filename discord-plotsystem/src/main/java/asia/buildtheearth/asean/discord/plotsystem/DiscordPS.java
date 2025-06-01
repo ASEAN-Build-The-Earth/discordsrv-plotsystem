9a796558b78f109894f97c825922ed96ca5b61c9
Tintinkung <sukrit_dinosuar@hotmail.com>
2025-06-02 02:57:05 +0700

    feat: deprecation notices v1.0.1
    We're moving groupId from `asia.buildtheearth.asean.discord.plotsystem` to `asia.buildtheearth.asean.discord` for simplification purpose.
    
    * Artifact ID and java package path remains the same.
    
    * Marked all POM file's description as deprecated
    
    * Marked DiscordPS class as deprecated on this version.

--- discord-plotsystem/src/main/java/asia/buildtheearth/asean/discord/plotsystem/DiscordPS.java
+++ discord-plotsystem/src/main/java/asia/buildtheearth/asean/discord/plotsystem/DiscordPS.java
@@ -43,8 +43,16 @@ import java.util.concurrent.*;
 
 import static github.scarsz.discordsrv.dependencies.kyori.adventure.text.Component.text;
 
+/**
+ * @deprecated This module has moved to:
+ *  groupId: asia.buildtheearth.asean.discord
+ *  artifactId: discord-plotsystem
+ *
+ * Please update your dependency to receive further updates.
+ */
+@Deprecated
 public final class DiscordPS extends DiscordPlotSystemAPI {
-    private static final String VERSION = "1.0.8";
+    private static final String VERSION = "1.0.1";
     private static final String DISCORD_SRV_VERSION = "1.29.0";
 
     /**
