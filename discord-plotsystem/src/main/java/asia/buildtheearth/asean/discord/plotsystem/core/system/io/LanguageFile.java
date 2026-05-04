d2454cc4ac8452801f19c03a83b10659fb56181a
Tintinkung <tintinkung.lemonade@gmail.com>
2026-05-05 01:17:13 +0700

    fix: LanguageFile#getEmbed not supporting null value
    * LanguageFile#getEmbed may collect [description] if the title is null
      when it is supposed to collect  [null, description] or [title, null]
    * Written simple test for that in LangTest.java

--- discord-plotsystem/src/main/java/asia/buildtheearth/asean/discord/plotsystem/core/system/io/LanguageFile.java
+++ discord-plotsystem/src/main/java/asia/buildtheearth/asean/discord/plotsystem/core/system/io/LanguageFile.java
@@ -5,8 +5,10 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class LanguageFile<T extends LangConfig> extends YamlConfiguration implements LangManager<T> {
 
@@ -27,10 +29,10 @@ public class LanguageFile<T extends LangConfig> extends YamlConfiguration implem
     }
 
     public @NotNull EmbedLang getEmbed(@NotNull String key, @Nullable String defaultValue) {
-        List<String> lang = this.getStringList(key);
+        List<String> lang = this.getLangPair(key);
 
-        String title = !lang.isEmpty() ? lang.getFirst() : defaultValue;
-        String description = lang.size() > 1 ? lang.get(1) : defaultValue;
+        String title = !lang.isEmpty() ? lang.getFirst() != null? lang.getFirst() : defaultValue : defaultValue;
+        String description = lang.size() > 1 ? lang.get(1) != null? lang.get(1) : defaultValue : defaultValue;
 
         return new EmbedLang(title, description);
     }
@@ -46,11 +48,14 @@ public class LanguageFile<T extends LangConfig> extends YamlConfiguration implem
     public @NotNull EmbedBuilder getEmbedBuilder(@NotNull T config,
                                         @NotNull Function<String, String> title,
                                         @NotNull Function<String, String> description) {
-        List<String> lang = this.getStringList(config.getKey());
+        List<String> lang = this.getLangPair(config.getKey());
         EmbedBuilder embed = new EmbedBuilder();
 
-        if(!lang.isEmpty()) embed.setTitle(title.apply(lang.getFirst()));
-        if(lang.size() > 1) embed.setDescription(description.apply(lang.get(1)));
+        if(!lang.isEmpty() && lang.getFirst() != null)
+            embed.setTitle(title.apply(lang.getFirst()));
+
+        if(lang.size() > 1 && lang.get(1) != null)
+            embed.setDescription(description.apply(lang.get(1)));
 
         return embed;
     }
@@ -62,6 +67,22 @@ public class LanguageFile<T extends LangConfig> extends YamlConfiguration implem
     public @NotNull EmbedBuilder getEmbedBuilder(@NotNull T config,
                                                  @NotNull Function<String, String> description) {
         return this.getEmbedBuilder(config, Function.identity(), description);
+    }
 
+    /**
+     * Extract nullable pair from language key
+     *
+     * @param key config key
+     * @return [null, null], [null, "value"], ["value", "value"], ["value", null]
+     */
+    @NotNull
+    private List<@Nullable String> getLangPair(String key) {
+        List<?> list = this.getList(key, Stream.of(null, null).toList());
+
+        // Produce [null, "value"], ["value", "value"], ["value", null]
+        List<@Nullable String> lang = new ArrayList<>();
+        for(Object obj : list) lang.add(obj == null ? null : obj.toString());
+
+        return lang;
     }
 }
---
+++ discord-plotsystem/src/test/java/asia/buildtheearth/asean/discord/plotsystem/test/LangTest.java
@@ -0,0 +1,41 @@
+package asia.buildtheearth.asean.discord.plotsystem.test;
+
+import asia.buildtheearth.asean.discord.plotsystem.DiscordPS;
+import asia.buildtheearth.asean.discord.plotsystem.core.system.io.LanguageFile;
+import asia.buildtheearth.asean.discord.plotsystem.core.system.io.lang.Notification;
+import asia.buildtheearth.asean.discord.plotsystem.test.mock.MockPluginServer;
+import github.scarsz.discordsrv.DiscordSRV;
+import org.junit.jupiter.api.*;
+import org.mockbukkit.mockbukkit.ServerMock;
+
+@DisplayName("Plugin")
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
+public class LangTest implements MockPluginServer {
+
+    protected static ServerMock server;
+    protected static DiscordPS plugin;
+    protected static github.scarsz.discordsrv.DiscordSRV discordSRV;
+
+    @Override
+    public void onServerStarted(ServerMock server, DiscordSRV discordSRV, DiscordPS plugin) {
+        PluginTest.server = server;
+        PluginTest.discordSRV = discordSRV;
+        PluginTest.plugin = plugin;
+    }
+
+    @Test
+    public void ErrorLangTest() {
+        for(Notification.ErrorMessage message : Notification.ErrorMessage.values()) {
+            LanguageFile.EmbedLang lang = DiscordPS.getSystemLang().getEmbed(message, null);
+
+            Assertions.assertNull(lang.title(), message.getClass().getName() + " is expected to have null title.");
+            Assertions.assertNotNull(lang.description(), message.getClass().getName() + " has null description lang.");
+        }
+    }
+
+    @AfterAll
+    @Override
+    public void onServerStop() {
+        MockPluginServer.super.onServerStop();
+    }
+}
